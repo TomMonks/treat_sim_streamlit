e89f7a611d8b95ff00afe8f38b37f27ab3027901
TomMonks <thomas.m.monks@gmail.com>
2022-08-02 14:12:45 +0100

    UPLOAD: download bug wkaround. Bokeh js event

--- pages/2_Upload_Experiments.py
+++ pages/2_Upload_Experiments.py
@@ -2,6 +2,14 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import pyperclip
+
+# widgets and JS events from bokeh to execute copy to clipboard
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+
+# return bokeh events from streamlit
+from streamlit_bokeh_events import streamlit_bokeh_events
 
 import model as md
 
@@ -46,13 +54,13 @@ def create_scenarios(df_scenarios):
 
     return cust_scenarios
 
-@st.cache
+@st.cache(show_spinner=False)
 def convert_df(df):
    return df.to_csv().encode('utf-8')
 
 @st.cache(show_spinner=False)
 def run_experiments(scenarios, n_reps):
-    return  md.run_scenario_analysis(cust_scenarios, 
+    return  md.run_scenario_analysis(scenarios, 
                                      md.DEFAULT_RESULTS_COLLECTION_PERIOD,
                                      n_reps)
 
@@ -60,8 +68,6 @@ def run_experiments(scenarios, n_reps):
 def results_as_summary_frame(results):
     return md.scenario_summary_frame(results).round(1)
 
-if 'executed' not in st.session_state:
-    st.session_state['executed'] = False
 
 st.title(TITLE)
 st.markdown(INFO_3)
@@ -79,38 +85,41 @@ if uploaded_file is not None:
     # loop through scenarios, create and run model
     n_reps = st.slider('Replications', 3, 30, 5, step=1)
     
-    button_txt = SHOW_TXT if st.session_state['executed'] else EXECUTE_TXT
-    if st.button(button_txt):
+    if st.button(EXECUTE_TXT):
 
         # create the cust scenarios based on upload
         cust_scenarios = create_scenarios(df_scenarios) 
         with st.spinner('Running scenario analysis'):
-            # results = md.run_scenario_analysis(cust_scenarios, 
-            #                                    md.DEFAULT_RESULTS_COLLECTION_PERIOD,
-            #                                    n_reps)
-            results = run_experiments(df_scenarios, n_reps)
+            results = run_experiments(cust_scenarios, n_reps)
             st.success('Done!')
             df_results = results_as_summary_frame(results)
-            # display in the app
+            # display in the app via table
             st.table(df_results)
-            st.session_state['executed'] = True
-            st.session_state.custom_results = convert_df(df_results)
             
 
-        # this cycles between working and 404 error...
+        # STREAMLIT BUG: this cycles between working and 404 error...
         st.download_button(
         "Download results as .csv",
-        st.session_state.custom_results,
+        convert_df(df_results),
         "experiment_results.csv",
         "text/csv",
         key='download-csv'
         )
 
-    
-
-    
-
-
+        # copy paste workaround for STREAMLIT Bug
+        # code based on https://discuss.streamlit.io/t/copy-dataframe-to-clipboard/2633
+        copy_button = Button(label="Copy results to clipboard")
+        copy_button.js_on_event("button_click", CustomJS(args=dict(df=df_results.to_csv(sep='\t')), code="""
+            navigator.clipboard.writeText(df);
+            """))
+            
+        no_event = streamlit_bokeh_events(
+            copy_button,
+            events="GET_TEXT",
+            key="get_text",
+            refresh_on_update=True,
+            override_height=75,
+            debounce_time=0)
     
 
     
